1c0cc4247fd2c78897e6ddd58a3ce9b6aeab49be
Lucca <lccmartina@gmail.com>
2023-06-29 13:33:16 -0300

    Pulimiento de ventanas inicio y menu

--- inventia-app/src/app/components/menu/menu.component.ts
+++ inventia-app/src/app/components/menu/menu.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  texto: string = 'Haz click en el modo de juego para ver su descripcion';
+  current: string = '';
+  validador: string = '';
+  constructor() {
 
+  }
+
+  selector(str: string, current: string) {
+    this.current = current;
+    console.log(this.validador)
+    console.log(this.current)
+    if(current === 'online' && this.validador === 'online-checked') {
+      console.log(this.validador)
+      this.texto = 'ermano checkado'
+      return
+    }
+    this.texto = str;
+  }
+
+  clickedOnline(){
+    this.texto = 'En el juego de modo online deberas escoger entre tres textos, dos generadas por una IA y una escrita por un humano, cual es la que piensas que realmente fue escrito por un humano.';
+  }
+  clickedOffline(){
+    this.texto = 'En el juego de modo offline se generara una situacion y sobre la misma tres testimonios generados por IA, uno de ellos es el correcto y tu deberas escogerlo.';
+
+  }
 }
